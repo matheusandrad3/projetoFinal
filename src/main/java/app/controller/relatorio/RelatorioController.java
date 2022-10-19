74c2fa1d0a488f8d306c4d8c16ce1a1f1b5bf5c2
otavio.SilvaN <otavio3397@gmail.com>
2022-10-19 16:13:37 -0300

    implementação relatorio

--- src/main/java/app/controller/relatorio/RelatorioController.java
+++ src/main/java/app/controller/relatorio/RelatorioController.java
@@ -1,7 +1,11 @@
 package app.controller.relatorio;
 
+import app.dto.relatorioDto.FiltroDataRequestDTO;
+import app.model.Pedidos;
 import app.model.Produto;
 import app.relatorio.Estoque;
+import app.relatorio.RelatorioPedidos;
+import app.repository.PedidosRepository;
 import app.repository.ProdutoRepository;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
@@ -12,6 +16,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,14 +31,17 @@ import java.util.Map;
 public class RelatorioController {
 
     @Autowired
-    private ProdutoRepository repository;
+    private ProdutoRepository produtoRepository;
+
+    @Autowired
+    private PedidosRepository pedidoRepository;
 
     @GetMapping("/produtos")
     public ResponseEntity<byte[]> gerarRelatorio() throws JRException, FileNotFoundException {
         List<Estoque> lista = new ArrayList<>();
         try {
 
-            for (Produto p : repository.findAll()) {
+            for (Produto p : produtoRepository.findAll()) {
                 Estoque e = new Estoque();
                 e.setNome(p.getNome());
                 e.setValorUnitario(p.getValorVenda());
@@ -60,6 +68,46 @@ public class RelatorioController {
             //set the PDF format
             headers.setContentType(MediaType.APPLICATION_PDF);
             headers.setContentDispositionFormData("filename", "Estoque.pdf");
+            //create the report in PDF format
+            return new ResponseEntity<byte[]>
+                    (JasperExportManager.exportReportToPdf(empReport), headers, HttpStatus.OK);
+
+        } catch (Exception e) {
+            return new ResponseEntity<byte[]>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/pedidos")
+    public ResponseEntity<byte[]> gerarRelatorioPedidos() throws JRException, FileNotFoundException {
+        List<RelatorioPedidos> lista = new ArrayList<>();
+        try {
+
+            for (Pedidos p : pedidoRepository.findAll()) {
+                RelatorioPedidos pedidos = new RelatorioPedidos();
+                pedidos.setDataCompra(p.getDataCompra().toString());
+                pedidos.setFormaPagamento(p.getFormaPagmento());
+                pedidos.setValorTotal(p.getValorTotal());
+                lista.add(pedidos);
+            }
+            Map<String, Object> empParams = new HashMap<String, Object>();
+            empParams.put("Araujo", "app");
+            empParams.put("RelatorioPedidos", new JRBeanCollectionDataSource(lista));
+
+            JasperPrint empReport =
+                    JasperFillManager.fillReport
+                            (
+                                    JasperCompileManager.compileReport(
+                                            ResourceUtils
+                                                    .getFile("classpath:Pedidos.jrxml")
+                                                    .getAbsolutePath()) // path of the jasper report
+                                    , empParams // dynamic parameters
+                                    , new JRBeanCollectionDataSource(lista)
+                            );
+
+            HttpHeaders headers = new HttpHeaders();
+            //set the PDF format
+            headers.setContentType(MediaType.APPLICATION_PDF);
+            headers.setContentDispositionFormData("filename", "Pedidos.pdf");
             //create the report in PDF format
             return new ResponseEntity<byte[]>
                     (JasperExportManager.exportReportToPdf(empReport), headers, HttpStatus.OK);
--- src/main/java/app/repository/PedidosRepository.java
+++ src/main/java/app/repository/PedidosRepository.java
@@ -2,6 +2,14 @@ package app.repository;
 
 import app.model.Pedidos;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PedidosRepository  extends JpaRepository<Pedidos, Long> {
+
+    /*@Query(name="Select * from tb_pedidos where data_compra >= :dataInicio AND data_compra <= :dataFinal", nativeQuery = true)
+    List<Pedidos> findAllPedidos(String dataInicio, String dataFinal);
+
+    */
 }
