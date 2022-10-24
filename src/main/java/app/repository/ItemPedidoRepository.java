bb2e9b6dc2cbc9332202252298dfd107a0b10efd
otavio.SilvaN <otavio3397@gmail.com>
2022-10-24 17:13:28 -0300

    implementação querys ordenar relatorio e de busca de produtos

--- src/main/java/app/repository/ItemPedidoRepository.java
+++ src/main/java/app/repository/ItemPedidoRepository.java
@@ -13,7 +13,7 @@ public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Long> {
     @Query("select new app.dto.relatorioDto.RelatorioItemPedidoResponseDTO" +
             "(ip.produto.nome, ip.pedidos.dataCompra, ip.valorUnitario, ip.quantidade, ip.valorTotal) " +
             "from ItemPedido ip where ip.pedidos.dataCompra BETWEEN :start AND :end " +
-            "AND ip.pedidos.statusPedido = 'CONCLUIDO' ")
+            "AND ip.pedidos.statusPedido = 'CONCLUIDO' order by ip.pedidos.id desc")
     List<RelatorioItemPedidoResponseDTO> recuperarItensPedido(LocalDate start, LocalDate end);
 
     @Query("from ItemPedido ip where ip.pedidos.id = :id and ip.produto.disponibilidade = 'DISPONIVEL'")
